d147ae0d26757f987a83d0f717bb9f8c15aed2ba
derp-dev <moonlaps3@gmail.com>
2023-08-11 22:45:43 -0500

    orca3 on every pull request, fam

---
+++ main/agents/repo_bot/repod.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, APIRouter, HTTPException, Request, Response
+from reviewer import review_pr
+
+router = APIRouter()
+
+@router.post("/github_webhooks")
+async def handle_github_webhooks(request: Request, response: Response):
+    print("Received a github webhook")
+    body = await request.json()
+    print(body)
+    action = body.get("action", None)
+    # number = body.get("number", None)
+    pull_request = body.get("pull_request", {})
+    if action == "opened" or action == "reopened":
+        html_url = pull_request.get("html_url", None)
+        if html_url is None:
+            return {}
+        try:
+            msg = review_pr(html_url)
+            print(msg)
+        except Exception as e:
+            print(e)
+    return {}
+        
+
+app = FastAPI()
+app.include_router(router, prefix="/api/v1")
