ade4a55a1d7d40d9019733331144d3bff4480513
derp-dev <moonlaps3@gmail.com>
2023-09-06 23:04:45 -0500

    gnu parallel(first-time, wow its awesome), bracks (semi-functional) ported over from cognsh

---
+++ main/app/bracks.py
@@ -0,0 +1,129 @@
+"""pseudocode
+// Variables 
+{{key}} // Placeholder
+${key} // Declared
+
+// Modifiers
+~ {{key}} // Approximate
++ {{key}} // Concatenate
+= {{key}} // Exact match 
+! {{key}} // Exclude
+- {{key}} // Subtract
+* {{key}} // Repeat
+>, <, <=, >= {{key}} // Comparisons
+
+// Conditionals  
+? {{key1}} : {{key2}} // If key1 then key2
+
+// Keywords
+ANY, ALWAYS, NEVER, WITH, AND, ONLY, NOT, UNIQUE
+
+// Functions
+between({{key1}}, {{key2}}) // Range
+random({{key1}}, ...) // Random
+mixed({{key1}}, ...) // Combination
+contains({{key}}) // Must contain  
+optimize({{key}}) // Optimize
+limit({{key}}, {{value}}) // Limit
+
+// Function Arguments
+fn(-{{key}}) // Remove 
+fn(*{{key}}) // Repeat
+fn(?{{key1}}:{{key2}}) // Conditional
+
+// Comments 
+// This is a comment
+
+// Includes
+include({{filename}})
+  
+// Math
+{{key1}} + {{key2}}  
+{{key1}} - {{key2}}
+{{key1}} * {{key2}}
+{{key1}} / {{key2}}
+
+// Nesting
+{{outerKey {{innerKey}}}}
+
+// Default values
+${key=defaultValue}
+
+// Data types  
+${key=123} // Number
+${flag=true} // Boolean
+${text="Hello"} // String
+
+// Object properties  
+{{person.name}}  
+{{person.age}}
+
+// JSON import
+import_json({{filename}})
+
+// Looping
+for {{i}} in {{array}} {
+  // loop body 
+}
+"""
+# Python Implementation Example
+
+# Define a function to process the custom syntax
+def process_custom_syntax(input_text, context={}):
+
+  # Split input into tokens
+  tokens = input_text.split()  
+
+  # Initialize output
+  output = ""
+
+  # Loop through tokens
+  for token in tokens:
+    
+    # Check if placeholder
+    if token.startswith("{{") and token.endswith("}}"):
+    
+      key = token[2:-2]  
+      if key in context:
+        output += context[key]
+      else:
+        output += token
+    
+    else:
+      output += token
+
+    # Add space after token  
+    output += " "
+
+  # Remove trailing space    
+  return output.strip()
+
+# Example usage
+custom_syntax = "This is a {{key1}} example {{key2}} with some {{key3}}."
+context = {
+  "key1": "custom",
+  "key2": "text", 
+  "key3": "placeholders"
+}
+
+result = process_custom_syntax(custom_syntax, context)
+print(result)
+"""
+    cat: Displays the contents of a file.
+    tac: Displays the contents of a file in reverse order.
+    more: Displays the contents of a file one page at a time.
+    less: Similar to more, but allows you to scroll through the file using the keyboard.
+    grep: Searches for a pattern in a file.
+    egrep: Extended version of grep that supports regular expressions.
+    fgrep: Fixed-string version of grep that only matches the exact pattern.
+    sed: Stream editor that can be used to edit text files.
+    awk: Pattern-matching language that can be used to extract data from text files.
+    sort: Sorts the lines of a file.
+    uniq: Removes duplicate lines from a file.
+    wc: Counts the lines, words, and characters in a file.
+    tee: Saves the output of a command to a file.
+    curl: Downloads the contents of a URL.
+    diff: Compares two files and displays the differences between them.
+    zcat: Displays the contents of a compressed file.
+    head: Displays the first few lines of a file.
+"""
